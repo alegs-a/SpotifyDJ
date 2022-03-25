57e4065056dc272cd562ffca928518a8ec734d74
Alexander Arthur <legsoflemurs@outlook.com>
2022-03-25 11:04:19 +1000

    Initial Commit

---
+++ SpotifyDJSwiftUI/SpotifyDJSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyDJSwiftUIApp.swift
+//  SpotifyDJSwiftUI
+//
+//  Created by Alex Arthur on 25/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyDJSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
